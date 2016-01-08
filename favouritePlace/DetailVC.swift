74bd002c40681d39fcb8f28d79b3f22d4f7712f6
Mehmet Ragıp Altuncu <mehmetaltuncu@gmail.com>
2016-01-08 02:05:23 -0800

    stupid app

---
+++ favouritePlace/DetailVC.swift
@@ -0,0 +1,38 @@
+//
+//  DetailVC.swift
+//  favouritePlace
+//
+//  Created by Mehmet Ragıp Altuncu on 08/01/16.
+//  Copyright © 2016 MehmetAltuncu. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backPressed(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
